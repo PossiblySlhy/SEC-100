ed67463c9c2c1e00c0c9dc7128b2590a7361ba40
Blake Phillips <db.phi@proton.me>
2025-01-06 15:47:31 -0600

    Updated notes

--- backup.py
+++ backup.py
@@ -3,35 +3,57 @@ import requests
 import apache_log_parser
 
 def fetch_log_entries(url):
-    ex2 = requests.get(url)
-    return ex2.text
+    text = requests.get(url)
+    return text
 
 def parse_logs(log_entries):
     # Parse log entries and return a list of dictionaries.
-    parts = log_entries.split(" ")
+    with open("logs.txt", "w") as logs:
+        logs.write(log_entries.text)
 
-    for i in
-    # Extract and concatenate the date part
-    date = parts[3] + ' ' + parts[4]
-    # Extract and concatenate the request line
-    request_line = ' '.join(parts[5:8])
-    # Create the dictionary with structured data
-    log_dict = {
-        'IP': parts[0],
-        'Identifier': parts[1],
-        'UserID': parts[2],
-        'Date': date,
-        'Request': request_line,
-        'Status': parts[8],
-        'Size': parts[9],
-        'Referer': parts[10],
-        'UserAgent': ' '.join(parts[11:])
-    }
-    return log_dict
+    # Prepare the parser
+    line_parser = apache_log_parser.make_parser("%a %l %u %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\"")
+
+    # List to hold parsed data
+    entries = []
+    try:
+        file = open(logs.txt, 'r')
+        for line in file:
+            parsed_data = line_parser(line)
+            entries.append(parsed_data)
+    except:
+        print("There was a problem parsing the file")
+        sys.exit(1)
+    finally:
+        file.close()
+
+    return log_entries
 
 def analyze_logs(logs):
-# Analyze logs to find specific user agent IP and the most popular status code.
-# FIX ME
+    # Analyze logs to find specific user agent IP and the most popular status code.
+
+    # Iterate through the logs to find the entry with the desired user agent
+    for log in logs:
+        if log.get("user_agent") == "Chrome/38.0.855.0":
+            print(f"IP Address: {log.get('ip')}")
+            break
+
+    status_count = {}
+    for entry in logs:
+        status = entry['status']
+        if status in status_count:
+            status_count[status] = status_count[status] + 1
+        else:
+            status_count[status] = 1
+
+    most_occur = max(status_count.values())
+    most_common_status_code = []
+    for status in status_count.keys():
+        if status_count[status] == most_occur:
+            most_common_status_code.append(status)
+
+    # Return a dictionary containing the result
+    return {'Most Frequent Status Code': most_common_status_code, 'Number of Occurences': most_occur}
 
 
 if __name__ == "__main__":
